955849e7b082cf074ed971e577f3f1f63276be14
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-03-27 18:38:17 -0600

    Agregar archivos de proyecto.

---
+++ L01P022022-SU-650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P022022_SU_650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
